d864376105994ce246ea517e93a7c24dad182bc2
rei <reiikedzero@gmail.com>
2022-11-27 02:26:05 +0900

    フォームhtml完了

--- WordPressTheme/front-page.php
+++ WordPressTheme/front-page.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 <main>
-  
-    <!-- メインビュー -->
-    <div class="p-mv">
-        <div class="p-mv__img">
-            <img src="<?php echo get_theme_file_uri('images/top/mv.jpg?ver=1.0.1'); ?>" alt="" />
-        </div>
-        <!-- PC,SPで画像切り替える場合 -->
-        <!-- <picture class="p-mv__img">
+
+  <!-- メインビュー -->
+  <div class="p-mv">
+    <div class="p-mv__img">
+      <img src="<?php echo get_theme_file_uri('images/top/mv.jpg?ver=1.0.1'); ?>" alt="" />
+    </div>
+    <!-- PC,SPで画像切り替える場合 -->
+    <!-- <picture class="p-mv__img">
           <source
             srcset="<?php echo get_theme_file_uri('images/top/mv_pc.jpg?ver=1.0.1'); ?>"
             media="(min-width: 768px)"
@@ -17,112 +17,17 @@
             alt=""
           />
         </picture> -->
-        <div class="p-mv__content">
-            <p class="p-mv__title">REIIWEB</p>
-            <p class="p-mv__text">お客様の夢を叶えるWebサイトを制作</p>
-        </div>
+    <div class="p-mv__content">
+      <p class="p-mv__title">REIIWEB</p>
+      <p class="p-mv__text">お客様の夢を叶えるWebサイトを制作</p>
     </div>
-    <!-- ../メインビュー -->
+  </div>
+  <!-- ../メインビュー -->
 
 
 
 
-    <section class="p-contact l-contact">
-        <div class="p-contact__inner l-inner">
-            <p class="p-contact__text">
-                お問い合わせ・ご相談等がございましたら、お気軽に下記コメント欄よりお寄せください。
-            </p>
-
-            <!-- セレクトボックス -->
-            <div class="p-contact__contents p-form">
-                <div class="p-form__content p-form__content--select">
-                    <div class="p-form__head">
-                        <label class="p-form__title">お問い合わせ内容</label>
-                        <span class="p-form__add">必須</span>
-                    </div>
-                    <div class="p-form__item p-form__item--select">
-                        <select name="content" class="p-form__select">
-                            <option value="" disabled selected>選択してください</option>
-                            <option value="">泥状掘削機について</option>
-                            <option value="">採用について</option>
-                            <option value="">その他</option>
-                        </select>
-                    </div>
-                </div>
-
-                <!-- ラジオボタン -->
-                <!-- <div class="p-form__content p-form__content--radio">
-                    <div class="p-form__head">
-                        <p class="p-form__title">お問い合わせの種類を選択してください</p>
-                        <span class="p-form__add">必須</span>
-                    </div>
-                    <div class="p-form__item">
-                        <div class="p-form__radios">
-                            <label>
-                                <input class="p-form__radio" type="radio" name="radio-name" /><span>商談のご相談</span>
-                            </label>
-                            <label>
-                                <input class="p-form__radio" type="radio" name="radio-name" /><span>サービスに関するお問い合わせ</span>
-                            </label>
-                            <label>
-                                <input class="p-form__radio" type="radio" name="radio-name" /><span>資料請求</span>
-                            </label>
-                            <label>
-                                <input class="p-form__radio" type="radio" name="radio-name" /><span>その他</span>
-                            </label>
-                        </div>
-                    </div>
-                </div> -->
-
-                <div class="p-form__content p-form__content--textarea">
-                    <div class="p-form__head">
-                        <label class="p-form__title" for="comment">コメント</label>
-                        <span class="p-form__add">必須</span>
-                    </div>
-                    <div class="p-form__item">
-                        <textarea class="p-form__textarea" id="comment"></textarea>
-                    </div>
-                </div>
-                <div class="p-form__content">
-                    <div class="p-form__head">
-                        <label class="p-form__title" for="name">お名前</label>
-                        <span class="p-form__add p-form__add--any">必須</span>
-                    </div>
-                    <div class="p-form__item">
-                        <input class="p-form__text" type="text" id="name" />
-                    </div>
-                </div>
-
-                <div class="p-form__content">
-                    <div class="p-form__head">
-                        <label class="p-form__title" for="name">お名前</label>
-                        <span class="p-form__add">必須</span>
-                    </div>
-                    <div class="p-form__item">
-                        <input class="p-form__text" type="text" id="name" />
-                    </div>
-                </div>
-
-                <!-- チェックボックス -->
-                <!-- <div class="p-form__privacy">
-                    <div class="p-form__head">
-                        <a href="#!" class="p-form__privacy-text">プライバシーポリシー</a>
-                        に同意の上、送信ください。
-                    </div>
-                    <div class="p-form__item">
-                        <label>
-                            <input class="p-form__check" type="checkbox" name="check-name" /><span>プライバシーポリシーに同意する</span>
-                        </label>
-                    </div>
-                </div> -->
-
-                <div class="p-form__btn">
-                    <input class="c-tn" type="submit" value="確認する" />
-                </div>
-            </div>
-
-        </div>
-    </section>
+  
 
 </main>
 <?php get_footer(); ?>
--- WordPressTheme/page-contact.php
+++ WordPressTheme/page-contact.php
@@ -7,46 +7,7 @@
                 お問い合わせ・ご相談等がございましたら、お気軽に下記コメント欄よりお寄せください。
             </p>
 
-            <!-- セレクトボックス -->
             <div class="p-contact__contents p-form">
-                <div class="p-form__content p-form__content--select">
-                    <div class="p-form__head">
-                        <label class="p-form__title">お問い合わせ内容</label>
-                        <span class="p-form__add">必須</span>
-                    </div>
-                    <div class="p-form__item p-form__item--select">
-                        <select name="content" class="p-form__select">
-                            <option value="" disabled selected>選択してください</option>
-                            <option value="">泥状掘削機について</option>
-                            <option value="">採用について</option>
-                            <option value="">その他</option>
-                        </select>
-                    </div>
-                </div>
-
-                <!-- ラジオボタン -->
-                <!-- <div class="p-form__content p-form__content--radio">
-                    <div class="p-form__head">
-                        <p class="p-form__title">お問い合わせの種類を選択してください</p>
-                        <span class="p-form__add">必須</span>
-                    </div>
-                    <div class="p-form__item">
-                        <div class="p-form__radios">
-                            <label>
-                                <input class="p-form__radio" type="radio" name="radio-name" /><span>商談のご相談</span>
-                            </label>
-                            <label>
-                                <input class="p-form__radio" type="radio" name="radio-name" /><span>サービスに関するお問い合わせ</span>
-                            </label>
-                            <label>
-                                <input class="p-form__radio" type="radio" name="radio-name" /><span>資料請求</span>
-                            </label>
-                            <label>
-                                <input class="p-form__radio" type="radio" name="radio-name" /><span>その他</span>
-                            </label>
-                        </div>
-                    </div>
-                </div> -->
 
                 <div class="p-form__content p-form__content--textarea">
                     <div class="p-form__head">
@@ -54,7 +15,7 @@
                         <span class="p-form__add">必須</span>
                     </div>
                     <div class="p-form__item">
-                        <textarea class="p-form__textarea" id="comment"></textarea>
+                        <textarea class="p-form__textarea" id="comment" placeholder="入力して下さい"></textarea>
                     </div>
                 </div>
                 <div class="p-form__content">
@@ -77,21 +38,60 @@
                     </div>
                 </div>
 
+                <!-- セレクトボックス -->
+                <!-- <div class="p-form__content">
+          <div class="p-form__head">
+            <label class="p-form__title">お問い合わせ内容</label>
+            <span class="p-form__add">必須</span>
+          </div>
+          <div class="p-form__item p-form__item--select">
+            <select name="content" class="p-form__select">
+              <option value="" disabled selected>選択してください</option>
+              <option value="">泥状掘削機について</option>
+              <option value="">採用について</option>
+              <option value="">その他</option>
+            </select>
+          </div>
+        </div> -->
+
+                <!-- ラジオボタン -->
+                <!-- <div class="p-form__content p-form__content--radio">
+          <div class="p-form__head">
+            <p class="p-form__title">選択してください</p>
+            <span class="p-form__add">必須</span>
+          </div>
+          <div class="p-form__item">
+            <div class="p-form__radios">
+              <label>
+                <input class="p-form__radio" type="radio" name="radio-name" /><span>商談のご相談</span>
+              </label>
+              <label>
+                <input class="p-form__radio" type="radio" name="radio-name" /><span>サービスに関するお問い合わせ</span>
+              </label>
+              <label>
+                <input class="p-form__radio" type="radio" name="radio-name" /><span>資料請求</span>
+              </label>
+              <label>
+                <input class="p-form__radio" type="radio" name="radio-name" /><span>その他</span>
+              </label>
+            </div>
+          </div>
+        </div> -->
+
                 <!-- チェックボックス -->
                 <!-- <div class="p-form__privacy">
-                    <div class="p-form__head">
-                        <a href="#!" class="p-form__privacy-text">プライバシーポリシー</a>
-                        に同意の上、送信ください。
-                    </div>
-                    <div class="p-form__item">
-                        <label>
-                            <input class="p-form__check" type="checkbox" name="check-name" /><span>プライバシーポリシーに同意する</span>
-                        </label>
-                    </div>
-                </div> -->
+          <p class="p-form__privacy-text">
+            <a href="#!">プライバシーポリシー</a>に同意の上、送信ください。
+          </p>
+          <div class="p-form__check-block">
+            <label>
+              <input class="p-form__check" type="checkbox" name="check-name" /><span>プライバシーポリシーに同意する</span>
+            </label>
+          </div>
+        </div> -->
 
                 <div class="p-form__btn">
-                    <input class="c-tn" type="submit" value="確認する" />
+                    <input class="c-btn" type="submit" value="確認する" />
                 </div>
             </div>
 
